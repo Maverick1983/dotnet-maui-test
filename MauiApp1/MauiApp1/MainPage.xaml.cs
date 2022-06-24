2ca39c3b85e6a37a8d095b36a003194e6e7e5695
Luca Scida <scida.luca@gmail.com>
2022-06-24 10:16:15 +0200

    First Commit

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		myWebView.Source = "https://play.famobi.com/smarty-bubbles-2/A-FAMOBI-COM";
+	}
+}
+
